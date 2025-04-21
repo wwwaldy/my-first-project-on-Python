53351558253d697b06a89a6082941439ae9d74ae
wwwaldy <kotovvladislav287@gmail.com>
2025-04-21 21:20:26 +0300

    Первая версия

---
+++ car_dealership.py
@@ -0,0 +1,24 @@
+import time
+
+audi_a_series = {
+    'a4': {
+        'brand': 'audi',
+        'model': 'a4',
+        'color': 'black',
+        'year': '2016',
+        'price': '19.500$',
+        'engine': '2.0 TFSI',
+        'mileage': '233.000 km',
+        'a5': {
+            'brand': 'audi',
+            'model': 'a5',
+            'color': 'white',
+            'year': '2018',
+            'price': '31.500$',
+            'engine': '2.0 TFSI',
+            'mileage': '80.000 km',
+        },
+    }
+}
+
+print('Добро пожаловать в автосалон!')
