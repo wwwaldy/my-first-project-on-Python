3a60bb1ee49528f291af9e0602b31ae88fb5c11f
wwwaldy <kotovvladislav287@gmail.com>
2025-05-21 15:28:14 +0300

    Поправил чувствительность к регистру, проверка ввода пользователя и проверка на по втор игры

--- coin/coin.py
+++ coin/coin.py
@@ -5,7 +5,11 @@ print("Орел или Решка?\n")
 
 def flip_coin():
     base = ["орел", "решка"]
-    user_choice = input("Выбери, Орел или Решка?: ")
+    user_choice = input("Выбери, Орел или Решка?: ").lower()
+    while user_choice not in base:
+        print("Введите корректное значение!\n")
+        time.sleep(1)
+        user_choice = input("Выберите еще раз, Орел или Решка?: ").lower()
     pc_choice = random.choice(base)
     print("Ты выбрал:", user_choice.capitalize())
     time.sleep(1)
@@ -16,16 +20,24 @@ def flip_coin():
     print("Монета падает...")
     time.sleep(1)
     print("\nМонета упала на:", pc_choice.capitalize())
+    time.sleep(0.5)
     if user_choice == pc_choice:
-        print("\nТы выиграл!")
+        print("Ты выиграл!")
+        time.sleep(1)
     else:
-        print("\nТы проиграл!")
+        print("Ты проиграл!")
+        time.sleep(1)
 
 def main():
     while True:
         flip_coin()
+        play_again_base = ["да", "нет"]
         play_again = input("\nХотите сыграть еще раз? Да/Нет: \n")
-        if play_again != "да":
+        while play_again.lower() not in play_again_base:
+            print("Введите корректное значение!\n")
+            time.sleep(1)
+            play_again = input("\nХотите сыграть еще раз? Да/Нет: \n")
+        if play_again.lower() != "да":
             time.sleep(0.5)
             print("Выход из игры...")
             time.sleep(0.5)
@@ -37,6 +49,10 @@ def main():
             time.sleep(0.5)
             print("Спасибо за игру!")
             break
+        else:
+            time.sleep(1)
+            print("Начинаем новую игру...\n")
+            time.sleep(1)
     
 if __name__ == "__main__":
     main()
